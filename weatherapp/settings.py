4c643343a2f2e31c7791c6ee303a08e240b12011
Atul Manjhi <atulmanjhu05@gmail.com>
2021-08-30 19:44:58 +0530

    second

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ax@d1b#o28kv^kcq2y+ozc29i#@j(y^x0+tx8r_+0m*lx+x!8'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weatherforecast05.herokuapp.com', 'localhost']
 
 
 # Application definition
